9d13d229ff5a7d90a7f13f096bf3691c83c488ef
Cauã Cristian Inocêncio <125278407+CauaCristian@users.noreply.github.com>
2024-08-02 10:28:06 -0300

    att

--- api_post_it/src/main/java/com/caua/api_post_it/repositories/UserRepository.java
+++ api_post_it/src/main/java/com/caua/api_post_it/repositories/UserRepository.java
@@ -1,5 +1,4 @@
 package com.caua.api_post_it.repositories;
-
 import com.caua.api_post_it.models.UserModel;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
